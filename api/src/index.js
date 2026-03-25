26a18ef9762bbbaf8f5c0c9a9e343f3346a20fe7
Oleh Barbashov <teosofio@gmail.com>
2026-03-25 17:11:12 +0000

    Added re-creation of the api service if it crashes. Added environment variables to specify the host and port.

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT
+const host = process.env.HOST
+
 app.get('/test', (req, res) => {
   res.send('Api: You are going to /test');
 });
 
-app.listen(3000, () => {
-  console.log('Api server is online');
+app.listen(port, () => {
+  console.log(`Api server is online on port ${port} and host ${host}`);
 });
